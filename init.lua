d09d1fc9304764591c8f27ff144c9aaa783e69c0
Axelsaft <alex@dev>
2025-08-19 19:44:08 +0200

    -edited config

--- init.lua
+++ init.lua
@@ -15,6 +15,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', 'gd', builtin.lsp_definitions, { desc = 'Telescope goto definitions' })
 --
 
 -- general
